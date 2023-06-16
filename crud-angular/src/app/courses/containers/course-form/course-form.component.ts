e8223ceb7467d644a4f5c2e5a95280d66b9f79c7
marcosjr <marcosjrpb@hotmailcom>
2023-06-16 17:48:55 -0300

    Validação do campos do formulario part 1

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
@@ -18,8 +18,8 @@ export class CourseFormComponent implements OnInit{
 
  form = this.formBuilder.group({
   _id: [''],
-  name: [''],
-  category:['']
+  name: ['',[Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+  category:['',Validators.required]
 });
 
 constructor(private formBuilder:  NonNullableFormBuilder,
@@ -57,6 +57,30 @@ ngOnInit(): void {
     onCancel(){
     this.location.back();
     }
+
+    getErrorMessage(fieldName: string) {
+      const field = this.form.get(fieldName);
+
+      if (field?.hasError('required')) {
+        return 'Campo Obrigatório';
+      }
+      if (field?.hasError('minlength')) {
+        const requiredLength = field.errors ?
+         field.errors['minlength']['requiredLength']:5;
+        return `Tamanho Minimo precisa ser  de ${requiredLength} caracteres`;
+      }
+      if (field?.hasError('maxlength')) {
+        const requiredLength = field.errors ?
+          field.errors['maxlength']['requiredLength'] : 5;
+        return `Tamanho Máximo precisa ser de ${requiredLength} caracteres`;
+      }
+
+
+
+      return 'Campo Inválido!';
+    }
+
+
 
 }
 
--- crud-angular/src/app/courses/containers/courses/courses.component.ts
+++ crud-angular/src/app/courses/containers/courses/courses.component.ts
@@ -57,7 +57,7 @@ export class CoursesComponent implements OnInit{
 
     });
 
-
+ 
   }
   ngOnInit(): void { }
 
